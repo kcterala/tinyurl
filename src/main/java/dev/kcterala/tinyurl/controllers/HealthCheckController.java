b8e23e72756c5ec5098259df58640a3616328f97
kcterala <kcterala@gmail.com>
2024-01-02 23:24:57 +0530

    Failing Build test

--- src/main/java/dev/kcterala/tinyurl/controllers/HealthCheckController.java
+++ src/main/java/dev/kcterala/tinyurl/controllers/HealthCheckController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutorService;
+
 @RestController
 @RequestMapping("/api/healthcheck")
 public class HealthCheckController {
     @GetMapping
     public ResponseEntity<String> healthCheck() {
+        System.out.println("Build should fail")
         return ResponseEntity.ok("Hi, How are you feeling today?");
     }
 }
