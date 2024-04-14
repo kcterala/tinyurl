b4cbbc377b691d7d68a1b36019d923382d59efa2
kcterala <kcterala@gmail.com>
2024-04-14 17:32:13 +0530

    Add Swagger configuration at the root domain

--- src/main/java/dev/kcterala/tinyurl/controllers/RedirectController.java
+++ src/main/java/dev/kcterala/tinyurl/controllers/RedirectController.java
@@ -3,6 +3,7 @@ package dev.kcterala.tinyurl.controllers;
 import dev.kcterala.tinyurl.exceptions.UrlNotFoundException;
 import dev.kcterala.tinyurl.interceptors.Unauthenticated;
 import dev.kcterala.tinyurl.services.UrlService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,13 @@ public class RedirectController {
     public ResponseEntity<Void> redirectToUrl(@PathVariable String shortCode) throws UrlNotFoundException {
         final String longUrl = urlService.getUrl(shortCode);
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                .location(URI.create(longUrl)).build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Void> redirectToSwagger() {
+        String longUrl =  "/swagger-ui/index.html"; // Assuming this is the Swagger UI endpoint
+        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
                 .location(URI.create(longUrl)).build();
     }
 
